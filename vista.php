4fb095eef4e6c7babec5b3fb730449b8e7901b48
César <gonzalezciclo@gmail.com>
2016-02-09 12:18:37 +0100

    Vista
    Subiendo Vista.php

---
+++ vista.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Listado de Artículos</title>
+  </head>
+  <body>
+    <h1>Listado de Artículos</h1>
+    <table>
+      <tr><th>Fecha</th><th>Título</th></tr>
+    <?php foreach ($articulos as $articulo): ?>
+      <tr>
+        <td><?php echo $articulo['fecha'] ?></td>
+        <td><?php echo $articulo['titulo'] ?></td>
+      </tr>
+    <?php endforeach; ?>
+    </table>
+  </body>
+</html>
